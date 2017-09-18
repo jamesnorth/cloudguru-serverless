b891cb246a14e7ac309e9e3730f9a94c1dcec7e7
James North <jamesnorth2104@gmail.com>
2017-09-18 10:51:25 +0100

    changed to run on AWS Lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,20 +4,25 @@ import mimetypes
 from io import BytesIO
 from zipfile import ZipFile
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.jrnlabs.co.uk')
-build_bucket = s3.Bucket('portfoliobuild.jrnlabs.co.uk')
+    portfolio_bucket = s3.Bucket('portfolio.jrnlabs.co.uk')
+    build_bucket = s3.Bucket('portfoliobuild.jrnlabs.co.uk')
 
-portfolio_zip = BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        if os.path.splitext(nm)[1] == '.map':
-            arg = {'ContentType': 'application/json'}
-        else:
-            args = {'ContentType': mimetypes.guess_type(nm)[0]}
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs=args)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            if os.path.splitext(nm)[1] == '.map':
+                arg = {'ContentType': 'application/json'}
+            else:
+                args = {'ContentType': mimetypes.guess_type(nm)[0]}
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs=args)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print("Job Done!")
+
+    return 'Hello from Lambda'
