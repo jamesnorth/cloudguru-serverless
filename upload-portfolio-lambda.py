fdcf8c27963d3922f5b790de7c447c7530f5dd62
James North <jamesnorth2104@gmail.com>
2017-09-18 19:59:17 +0100

    modifications to deployment script to use CodePipeline

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,14 +4,16 @@ import mimetypes
 from io import BytesIO
 from zipfile import ZipFile
 
-def lambda_handler(event, context):
+def build_portfolio(bucket_name, object_key):
     s3 = boto3.resource('s3')
 
+    print('Building from {} with {}'.format(bucket_name, object_key))
+
     portfolio_bucket = s3.Bucket('portfolio.jrnlabs.co.uk')
-    build_bucket = s3.Bucket('portfoliobuild.jrnlabs.co.uk')
+    build_bucket = s3.Bucket(bucket_name)
 
     portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    build_bucket.download_fileobj(object_key, portfolio_zip)
 
     with ZipFile(portfolio_zip) as myzip:
         for nm in myzip.namelist():
@@ -22,6 +24,30 @@ def lambda_handler(event, context):
                 args = {'ContentType': mimetypes.guess_type(nm)[0]}
             portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs=args)
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+def lambda_handler(event, context):
+    job = event.get('CodePipeline.job')
+
+    location = {
+        'bucketName': 'portfoliobuild.jrnlabs.co.uk',
+        'objectKey': 'portfoliobuild.zip'
+    }
+
+    if job is not None:
+        for artifact in job['data']['inputArtifacts']:
+            if artifact['name'] == 'MyAppBuild':
+                location = artifact['location']['s3Location']
+        code_pipeline = boto3.client('codepipeline')
+    else:
+        code_pipeline = None
+
+    try:
+        build_portfolio(location['bucketName'], location['objectKey'])
+
+        if code_pipeline is not None:
+            code_pipeline.put_job_success_result(jobId=job['id'])
+    except:
+        pass
 
     print("Job Done!")
 
